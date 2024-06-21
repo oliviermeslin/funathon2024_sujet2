d4ed1ca1b91c1d78ffa30feb29956342d9d1082e
oliviermeslin <olivier.meslin@insee.fr>
2024-06-21 10:04:53 +0000

    WIP, partie 3 finie

---
+++ src/clean_dataframe.py
@@ -0,0 +1,11 @@
+def clean_dataframe(df):
+
+    df["an"] = df["ANMOIS"].str.slice(stop = 4)
+    df["mois"] = df["ANMOIS"].str.slice(start = 4)
+
+    # Remove leading zeros from 'mois' column
+    df['mois'] = df['mois'].str.replace(r'^0+', '', regex=True)
+    
+    data.columns = [x.lower() for x in data.columns]
+
+    return(df)
